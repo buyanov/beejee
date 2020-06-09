bae6eaa6510cc0c69c57c68b7d5d089cde16b8a7
Buyanov Danila <info@saity74.ru>
2020-06-09 18:04:02 +0500

    Add DB

--- config/di.php
+++ config/di.php
@@ -1,5 +1,7 @@
 <?php
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +11,32 @@ use Twig\Loader\FilesystemLoader;
 
 return [
 
+    'db.config' => static function() {
+        $isDevMode = true;
+        $proxyDir = null;
+        $cache = null;
+        return Setup::createAnnotationMetadataConfiguration(
+            [__DIR__."/../src/TaskManager/Models"],
+            $isDevMode,
+            $proxyDir,
+            $cache,
+            false
+        );
+    },
+
+    EntityManager::class => static function(ContainerInterface $c) {
+        $conn = [
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/../db.sqlite',
+        ];
+
+        return EntityManager::create($conn, $c->get('db.config'));
+    },
+
+    'db.entity_manager' => DI\get(EntityManager::class),
+
     Application::class => static function(ContainerInterface $container) {
+        Request::enableHttpMethodParameterOverride();
         $request = Request::createFromGlobals();
         $response = new Response();
         return new Application($request, $response, $container);
@@ -21,4 +48,5 @@ return [
         $loader = new FilesystemLoader(__DIR__ . '/../src/TaskManager/Views');
         return new Environment($loader);
     },
+
 ];
